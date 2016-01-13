1e51c63a84a4f950a1f4e704bd426b9d385d30c9
WilliamGec <william.gec@gmail.com>
2016-01-13 16:51:02 -0500

    arreglo de favicon

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,3 +2,30 @@ document.addEventListener('DOMContentLoaded',function(){
 $('.menu-mobil').click(function (){$(this).next().slideToggle();})
 
 })
+
+
+	var clickEvent = document.getElementById('enviar');
+	var mensaje = document.getElementById('mensaje__exito');
+	console.log(mensaje.classList);
+	clickEvent.addEventListener('click',function () {
+		mensaje.classList.toggle('mostrar__mensaje-contacto');
+	})
+
+
+/*document.addEventListener('DOMContentLoaded',function(){
+	var correo = document.getElementById(correo);
+	var stop = 0;
+	var nombre ="";
+	console.log(correo.length-1 + correo.charAt(0));
+	for (var i = 0; i < correo.length-1; i++) {
+		if(correo.charAt(i)==='@'){
+			stop = i;
+		}
+	};
+	for (var i = 0; i < stop; i++) {
+		nombre=nombre+correo.charAt(i);
+	};
+	
+
+			console.log (stop + " "+ nombre); 
+});*/
