c7343b2c9c08750c204ca2a70136b2556826e964
WilliamGec <william.gec@gmail.com>
2016-01-22 15:47:24 -0500

    Se ingresa autoprefixer para sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -10,6 +11,10 @@ gulp.task('default', function() {
 gulp.task('sass', function () {
   gulp.src('./public/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
     .pipe(sass({outputStyle:'compressed'}))
     .pipe(gulp.dest('./public/stylesheets/'));
 });
